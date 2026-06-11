02284d42de9517ae0549f8fb0b06794551c53f59
keonshu@gmail.com <keonplaysguitar@gmail.com>
2026-06-11 12:29:19 +0800

    Add Id to OrderItemDTO so refund request can reference order item

--- Ecoture/Model/DTO/OrderItemDTO.cs
+++ Ecoture/Model/DTO/OrderItemDTO.cs
@@ -2,6 +2,7 @@
 {
     public class OrderItemDTO
     {
+        public int Id { get; set; }
         public int ProductId { get; set; }
         public required string ProductTitle { get; set; }
         public decimal Price { get; set; }
